3fee9b9650180d0bd859fb04521d4312f988003a
Hassan Shan <hassanshanqsols@gmail.com>
2022-12-24 14:39:06 +0500

    navbar needs responsivness

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,30 +1,56 @@
 import Link from "next/link";
 import React from "react";
 
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="fixed left-0 top-0 w-full z-10 ease-in-out duration-300">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white ">
         <Link href="/">
           <h1>Captur</h1>
         </Link>
-        
+
         <ul>
-            <li>
-                <Link href="/">Home</Link>
-            </li>
-            <li>
-                <Link href="/gallery">Gallery</Link>
-            </li>
-            <li>
-                <Link href="/portfolio">Work</Link>
-            </li>
-            <li>
-                <Link href="/contact">Contact</Link>
-            </li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Work</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
 
         {/* add in reacct icons and mobile menu */}
+
+        {/* Mobile Button */}
+        <div>
+          <AiOutlineMenu size={20} />
+          {/* <AiOutlineClose size={20}/> */}
+        </div>
+
+        {/* Mobile Menu */}
+        <div>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/portfolio">Work</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
