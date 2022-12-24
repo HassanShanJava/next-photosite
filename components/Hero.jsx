dce984c7f846563a161167ec7090410b26a7b8db
Hassan Shan <hassanshanqsols@gmail.com>
2022-12-24 13:51:14 +0500

    working on first next porject with new it skills

---
+++ components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      {/* overlay 4 parallax backround */}
+      <div />
+
+      {/* title */}
+      <div>
+        <h2>Heading</h2>
+        <p>Measage</p>
+        <button>Book</button>
+      </div>
+
+      
+
+
+      
+    </div>
+  )
+}
+
+export default Hero
